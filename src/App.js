ccc2bd9b0d443e2847cbd7b52ece323665788d79
Heyswor <andreydymov93@gmail.com>
2023-07-04 01:37:18 +0300

    1234

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import LevelCalculator from "./components/LevelCalculator/LevelCalculator";
 import GlyphCalc from "./components/GlyphCalc/GlyphCalc";
 import "./App.css";
-import { Routes, Route, NavLink } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <NavLink
-          to="/diablotablet"
-          className="navLink"
-          activeClassName="active"
-        >
-          LevelCalculator
-        </NavLink>
-        <NavLink to="/glyphcalc" className="navLink" activeClassName="active">
-          Glyph Calculator
-        </NavLink>
-      </nav>
-      <Routes>
-        <Route path="/diablotablet" element={<LevelCalculator />} />
-        <Route path="/glyphcalc" element={<GlyphCalc />} />
-      </Routes>
-    </div>
+    <Router basename="/diablotablet">
+      <div>
+        <nav>
+          <NavLink to="/" className="navLink" activeClassName="active">
+            LevelCalculator
+          </NavLink>
+          <NavLink to="/glyphcalc" className="navLink" activeClassName="active">
+            Glyph Calculator
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LevelCalculator />} />
+          <Route path="/glyphcalc" element={<GlyphCalc />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
